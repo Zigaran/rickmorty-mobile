ac11f49be800525498083b27590e62d7c99e5e7f
Zigaran <zigaraneliasnatan@gmail.com>
2020-09-21 12:53:39 -0300

    Husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
   root: true,
-  extends: [
-    '@react-native-community',
-    'airbnb-typescript',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'prettier/react',
-  ],
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
 };
